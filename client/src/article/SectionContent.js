c5bd73a9f56ec4beab49094882e959b72df4b4ef
atar13 <atar13@users.noreply.github.com>
2021-03-21 04:07:00 +0000

    deploy: c0a92114d9bd39776627c392d7bdb73b0dd9c58e

--- client/src/article/SectionContent.js
+++ client/src/article/SectionContent.js
@@ -10,6 +10,11 @@ import './Article.css';
 
 const localizer = momentLocalizer(moment)
 
+let sectionEvents = [{
+    start: moment("08:00 AM 2021-02-24", "hh:mm A YYYY-MM-DD").toDate(),
+    end: moment("09:00 AM 2021-02-24", "hh:mm A YYYY-MM-DD").toDate(),
+    title: "Lecture"
+}];
 
 /**
  * React component for each courses' sections 
@@ -17,19 +22,10 @@ const localizer = momentLocalizer(moment)
  */
 function SectionContent(props) {
     const { section, course } = props;
-    let sectionEvents = [];
-    let initWeek = 2;
-    for (let index = initWeek; index < initWeek + 10; index++) {
-        sectionEvents.push({
-            start: moment(`08:00 AM Mon ${index}`, "hh:mm A ddd w").toDate(),
-            end: moment(`09:00 AM Mon ${index}`, "hh:mm A ddd w").toDate(),
-            title: "Lecture"
-        })
-    }
     return (
         <div>
             <h1 className="article-body">{course.name}: {section.professor} {section.letter}</h1>
-            <div id="section-content">
+            <div id="section-content" >
                 <div>
                     <div id="section-info">
                         <h3>Section: <span>{section.letter}</span></h3>
@@ -41,8 +37,7 @@ function SectionContent(props) {
                     </button>
                     <p className="section-description">
                         CSE11 UCSD Cao science computer Cao section Objectorientedlanguage Cao CSE11 Miranda Objectorientedlanguage. Cao Gary Winter Fall Cao 2021 CSE11 discussion section. Winter Miranda Cao Java Cao 2021 Gary Cao Java Miranda Cao Miranda discussion. Cao 2020 CSE11 UCSD Cao Gary UCSD. Fall 2021 2020 UCSD section computer Cao section Fall Cao CSE11 computer 2020 2021?
-                    </p>
-                    <h2 className="section-schedule">Schedule:</h2>
+                </p>
                     <div className="section-calendar">
                         <Calendar
                             localizer={localizer}
