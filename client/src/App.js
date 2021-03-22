eee1f1a97a763144b8ff1957b47df475a108f2b8
tristinxie <tristinxie@users.noreply.github.com>
2021-03-22 01:35:15 +0000

    deploy: 2c44eb70e545117f76d0d6776091aff48ecde37f

--- client/src/App.js
+++ client/src/App.js
@@ -18,44 +18,38 @@ import {
   // Redirect,
 } from 'react-router-dom';
 
-import './App.css';
-import UserProfile from './userProfile/UserProfile';
-
 function App() {
 
   return (
-    <div className="app-container">
+    <div>
       <Router>
         <TopBar />
         <div className="main-container">
-          <Navbar />
-          <div className="page-container">
-            <Switch>
-              <Route exact path='/' component={home} />
-              <Route exact path='/createPageClass' component={createPageClass} />
-              <Route exact path='/createPageOrg' component={createPageOrg} />
-              <Route exact patch='/userProfile' component={UserProfile} />
-              {dummyArticles.map((course) => (
-                <Route exact
-                  path={`/courses/${course.department}/${course.name}`}
-                  children={<Article course={course}>
-                    <CourseContent course={course}></CourseContent>
-                  </Article>}>
-                </Route>
-              ))}
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={home} />
+          <Route exact path='/createPageClass' component={createPageClass} />
+          <Route exact path='/createPageOrg' component={createPageOrg} />
+          {dummyArticles.map((course) => (
+            <Route exact
+              path={`/courses/${course.department}/${course.name}`}
+              children={<Article course={course}>
+                <CourseContent course={course}></CourseContent>
+              </Article>}>
+            </Route>
+          ))}
 
-              {dummyArticles.map((course) => (
-                course.sections.map((section) => (
-                  <Route exact
-                    path={`/courses/${course.department}/${course.name}/${section.professor}`}
-                    children={<Article section={section} course={course} >
-                      <SectionContent section={section} course={course}></SectionContent>
-                    </Article>}>
-                  </Route>
-                ))
-              ))}
-            </Switch>
-          </div>
+          {dummyArticles.map((course) => (
+            course.sections.map((section) => (
+              <Route exact
+                path={`/courses/${course.department}/${course.name}/${section.professor}`}
+                children={<Article section={section} course={course} >
+                  <SectionContent section={section} course={course}></SectionContent>
+                </Article>}>
+              </Route>
+            ))
+          ))}
+        </Switch>
         </div>
       </Router>
     </div>
