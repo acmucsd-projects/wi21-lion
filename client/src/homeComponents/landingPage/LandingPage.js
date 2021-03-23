6c1c2422bf2510708a85d8bda0dfe6a6d12d8a8c
Anthony Tarbinian <atar137h@gmail.com>
2021-03-23 14:08:24 -0700

    Landing page link outline

--- client/src/homeComponents/landingPage/LandingPage.js
+++ client/src/homeComponents/landingPage/LandingPage.js
@@ -10,12 +10,12 @@ const LandingPage = () => {
             <h2>UCSD</h2>
             <h1>Wiki</h1>
             <div className="landing-icon-container">
+                <HomeSVG className="landing-svg" />
                 <Link exact to="/orgs/ACM" style={{ position: 'absolute', top: '0%', right: '60%' }}>ACM</Link>
                 <Link exact to="/orgs/WIC" style={{ position: 'absolute', top: '20%', right: '70%' }}>WIC</Link>
                 <Link exact to="/orgs/TritonUAS" style={{ position: 'absolute', top: '40%', right: '72%' }}>TritonUAS</Link>
                 <Link style={{ position: 'absolute', top: '60%', right: '70%' }}>SEDS</Link>
                 <Link style={{ position: 'absolute', top: '80%', right: '60%' }}>CS foreach</Link>
-                <HomeSVG className="landing-svg" />
                 <Link exact to="/courses/CSE/CSE11" style={{ position: 'absolute', top: '0%', left: '60%' }}>CSE 11</Link>
                 <Link exact to="/courses/CSE/CSE12" style={{ position: 'absolute', top: '20%', left: '70%' }}>CSE 12</Link>
                 <Link style={{ position: 'absolute', top: '40%', left: '72%' }}>Math 20A</Link>
