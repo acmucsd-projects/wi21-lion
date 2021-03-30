2ac0958a8c1c042186657f286a457d10e5fe76ab
atar13 <atar13@users.noreply.github.com>
2021-03-30 07:36:57 +0000

    deploy: 897ff9f300046251ce302a3289f12249456b3f36

--- client/src/article/CourseContent.js
+++ client/src/article/CourseContent.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import { useState } from "react";
+import { EditCourse } from "./EditArticle";
 
 function CourseContent(props) {
 
     const { course } = props;
+    const [displayEditCourse, setDisplayEditCourse] = useState(false);
+
+    function showEditCourse() {
+        setDisplayEditCourse(true);
+    }
+
+    function hideEditCourse() {
+        setDisplayEditCourse(false);
+    }
+
+    function handleClickOff(event) {
+        if(event.target.className === "edit-backdrop") {
+            hideEditCourse();
+        }
+    }
+
 
     return (
         <div>
-            <h1 className="article-body">{course.name + ": " + course.shortDescription}</h1>
-            <div className="article-body">
-                <p className="long-description">
-                    {course.longDescription}
-                </p>
+            <div className="section-content-header article-body">
+            <h1>{course.name}</h1>
+            <button className="edit-button" onClick={showEditCourse}>
+                <span>Edit</span>
+            </button>
             </div>
+            <div className="article-inner-body">
+                <p className="long-description">
+                    {course.description}
+                </p>
+                <img className="course-img" src={course.image} alt={course.name}></img>
+            </div>
+            {displayEditCourse && <div className="edit-backdrop" onClick={handleClickOff}>
+                <div className="edit-section-wrapper" >
+                <EditCourse course={course} closeCourseEdit={hideEditCourse}/>
+            </div>
+            </div>}
         </div>
     )
 }
