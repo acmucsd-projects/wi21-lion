1c5368b937ebe7c2bf72411ee2163a6d2a5f65f6
tristinxie <tristinxie@users.noreply.github.com>
2021-03-30 04:03:48 +0000

    deploy: ddfd6e87eff3aa810fd45ba786d375cbbdd7c96a

--- client/src/homeComponents/landingPage/LandingPage.js
+++ client/src/homeComponents/landingPage/LandingPage.js
@@ -1,26 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { LoginDialog, SignupDialog } from '../../popups/dialogs';
 import './LandingPage.css'
 import { ReactComponent as HomeSVG } from './res/home.svg';
 
 const LandingPage = () => {
-    //TODO: change to a json file 
+    const [loginDialogShown, setShowLoginDialog] = useState(false);
+    const [signupDialogShown, setShowSignupDialog] = useState(false);
+    function hideLoginDialog() { setShowLoginDialog(false); }
+    function hideSignupDialog() { setShowSignupDialog(false); }
+    function showLoginDialog() { setShowLoginDialog(true); }
+    function showSignupDialog() { setShowSignupDialog(true); }
     return (
         <div className="landing-container">
-            <h2>UCSD</h2>
-            <h1>Wiki</h1>
+            <h1>UCSD</h1>
+            <h2>Wiki</h2>
             <div className="landing-icon-container">
                 <HomeSVG className="landing-svg" />
-                <Link exact to="/orgs/ACM" style={{ position: 'absolute', top: '0%', right: '60%' }}>ACM</Link>
-                <Link exact to="/orgs/WIC" style={{ position: 'absolute', top: '20%', right: '70%' }}>WIC</Link>
-                <Link exact to="/orgs/TritonUAS" style={{ position: 'absolute', top: '40%', right: '72%' }}>TritonUAS</Link>
-                <Link style={{ position: 'absolute', top: '60%', right: '70%' }}>SEDS</Link>
-                <Link style={{ position: 'absolute', top: '80%', right: '60%' }}>CS foreach</Link>
-                <Link exact to="/courses/CSE/CSE11" style={{ position: 'absolute', top: '0%', left: '60%' }}>CSE 11</Link>
-                <Link exact to="/courses/CSE/CSE12" style={{ position: 'absolute', top: '20%', left: '70%' }}>CSE 12</Link>
-                <Link style={{ position: 'absolute', top: '40%', left: '72%' }}>Math 20A</Link>
-                <Link style={{ position: 'absolute', top: '60%', left: '70%' }}>ECE 45</Link>
-                <Link style={{ position: 'absolute', top: '80%', left: '60%' }}>BILD 3</Link>
+                <Link exact to="/courses" style={{ position: 'absolute', top: '20%', right: '70%' }}>Courses</Link>
+                <Link exact to="/createPageClass" style={{ position: 'absolute', top: '40%', right: '72%' }}>Create Course</Link>
+                <Link exact to="/orgs" style={{ position: 'absolute', top: '60%', right: '70%' }}>Orgs</Link>
+                <Link onClick={showLoginDialog} style={{ position: 'absolute', top: '20%', left: '70%' }}>Login</Link>
+                <Link onClick={showSignupDialog} style={{ position: 'absolute', top: '40%', left: '72%' }}>Register</Link>
+                <Link exact to="/userProfile" style={{ position: 'absolute', top: '60%', left: '70%' }}>Profile</Link>
+                <Link exact to="/createPageOrg" style={{ position: 'absolute', top: '80%', right: '60%' }}>Create Org</Link>
+                <Link exact to="/orgs/ACM" style={{ position: 'absolute', top: '80%', left: '60%' }}>ACM</Link>
+            </div>
+            <div style={{ position: "absolute", top: 0 }}>
+                {loginDialogShown && <LoginDialog show={showLoginDialog} hide={hideLoginDialog} />}
+                {signupDialogShown && <SignupDialog show={showSignupDialog} hide={hideSignupDialog} />}
             </div>
         </div>
     )
